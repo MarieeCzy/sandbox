6be7e6a542240bfc8a3a28b300ea54e5a7a40aae
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-03-14 17:41:30 +0100

    Add pl_asos network, download data from web with for loop

--- load_data.sh
+++ load_data.sh
@@ -16,18 +16,25 @@ START_YEAR=$2 #2022
 
 END_DAY=31
 END_MONTH=12
-END_YEAR=${START_YEAR}
+END_YEAR=$3 #${START_YEAR}
 
-URL_PREFIX="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+#https://www.cyberciti.biz/faq/unix-linux-iterate-over-a-variable-range-of-numbers-in-bash/
 
-LOCAL_PREFIX="data/raw/${STATION}/${START_YEAR}"
-LOCAL_FILE="${STATION}_${FSTART_DAY}_${FSTART_MONTH}_${START_YEAR}_${END_DAY}_${END_MONTH}_${END_YEAR}.csv"
-LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+for ((c=$START_YEAR; c<=$END_YEAR; c++)); do
 
-URL="${URL_PREFIX}station=${STATION}&data=all&year1=${START_YEAR}&month1=${START_MONTH}&day1=${START_DAY}&year2=${END_YEAR}&month2=${END_MONTH}&day2=${END_DAY}&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=yes&missing=null&trace=T&direct=no&report_type=3&report_type=4"
+    URL_PREFIX="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
 
-mkdir -p ${LOCAL_PREFIX}
-wget ${URL} -O ${LOCAL_PATH}
+    LOCAL_PREFIX="data/raw/${STATION}/${c}"
+    LOCAL_FILE="${STATION}_${FSTART_DAY}_${FSTART_MONTH}_${c}_${END_DAY}_${END_MONTH}_${c}.csv"
+    LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+    URL="${URL_PREFIX}station=${STATION}&data=all&year1=${c}&month1=${START_MONTH}&day1=${START_DAY}&year2=${c}&month2=${END_MONTH}&day2=${END_DAY}&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=yes&missing=null&trace=T&direct=no&report_type=3&report_type=4"
+
+    mkdir -p ${LOCAL_PREFIX}
+    wget ${URL} -O ${LOCAL_PATH}
+    sleep 10
+done
+
 
 #chmod +x load_data.sh
 
